fe9245632a77fa15aa53df8f91111b4fdca5dd25
terryc <terryc9001@gmail.com>
2022-12-09 15:03:07 +0800

    first changes attempts
    first changes attempts

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
  * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
  * Let's keep the sky-high status of this song untouched!
  *
- * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
+ * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal.
  * Choose a heart-touching track! 🎵 ❤️
  *
  * Listen here: https://youtu.be/X0MDALpV29s
@@ -26,34 +26,34 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("March 11, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     // Output the result in an element with id="demo"
     document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
+
+    // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
 
-// being a bit cool :p  
+// being a bit cool :p
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
@@ -87,7 +87,7 @@ var styles2 = [
 
 console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+console.log('%cYour presence is requested!%c\n\nRegards: Terry & Wendee', styles1, styles2);
 
 console.log(
     `%cShaadi me zaroor aana!\n\n`,
