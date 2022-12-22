a378d88bfc948fda967604db7000301ce200b85e
terryc <terryc9001@gmail.com>
2022-12-22 23:28:11 +0800

    date

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("March 11, 2023 00:00:00").getTime();
+var countDownDate = new Date("March 10, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
